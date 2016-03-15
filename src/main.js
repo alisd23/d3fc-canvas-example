26419ec34999f868766a78c0d5fa771be698d940
Ali Sheehan-Dare <alisd23@live.co.uk>
2016-03-15 17:33:14 +0000

    fix: Fix broken merges

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import fc from 'd3fc';
-<<<<<<< HEAD
 import d3 from 'd3';
 import { scaleLinear, scaleTime } from 'd3-scale';
-=======
-import d3 from 'd3'
->>>>>>> 1b1b7ec... Finished
 
 import candlestickSeries from './candlestickSeries';
 import '../sass/app.scss';
@@ -13,7 +9,6 @@ const canvasEl = document.getElementById('canvas');
 const svgEl = document.getElementById('svg');
 const selectEl = document.getElementById('points');
 
-<<<<<<< HEAD
 window.addEventListener('load', function() {
   d3.select(svg)
     .append('path')
@@ -97,53 +92,3 @@ window.addEventListener('load', function() {
           .range([height, 0]);
   }
 });
-=======
-const width = document.body.clientWidth;
-const height = svgEl.clientHeight;
-canvasEl.width = width;
-
-d3.select(svg)
-  .attr('width', width)
-  .append('path')
-  .attr('class', 'up');
-
-d3.select(svg)
-  .append('path')
-  .attr('class', 'down');
-
-const data = createData(Number(selectEl.value));
-
-const series = candlestickSeries();
-
-d3.selectAll('.chart')
-  .datum(data)
-  .call(series);
-
-// Listen for dropdown change
-selectEl.addEventListener('change', () => {
-  const newData = createData(Number(selectEl.value));
-
-  d3.selectAll('.chart')
-    .datum(newData)
-    .call(series);
-});
-
-
-/**
- * Data creation function
- * @param  {Number}   count - Number of data points
- * @return {Object[]} data
- */
-function createData(count) {
-  const ohlcDataGenerator = fc.data.random.financial()
-      .startDate(new Date(2014, 1, 1));
-
-  const data = ohlcDataGenerator(count);
-
-  return {
-    all: data,
-    up: data.filter(d => d.open <= d.close),
-    down: data.filter(d => d.open > d.close)
-  };
-}
->>>>>>> 1b1b7ec... Finished
